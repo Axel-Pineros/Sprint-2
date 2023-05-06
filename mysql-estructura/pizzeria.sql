be92b531a7cd2357427ba2adf9d9aefc3ecea650
Axel Piñeros <axelpineros@outlook.com>
2023-05-06 21:30:04 +0200

    pizzeria feta

--- mysql-estructura/pizzeria.sql
+++ mysql-estructura/pizzeria.sql
@@ -5,18 +5,16 @@
 --     Per a cada client/a emmagatzemem un identificador únic: Nom, cognoms, adreça, codi postal, localitat, província, número de telèfon.
 --     Una persona pot realitzar moltes comandes, però una única comanda només pot ser realitzat per una única persona. De cada comanda s'emmagatzema un identificador únic: Data/hora, si la comanda és per a repartiment a domicili o per a recollir en botiga, la quantitat de productes que s'han seleccionat de cada tipus, el preu total.
 
-
 -- Una comanda pot constar d'un o diversos productes.
 
 --     Els productes poden ser pizzes, hamburgueses i begudes. De cada producte s'emmagatzema un identificador únic: Nom, descripció, imatge, preu.
-
 
 -- En el cas de les pizzes existeixen diverses categories que poden anar canviant de nom al llarg de l'any. Una pizza només pot estar dins d'una categoria, però una categoria pot tenir moltes pizzes.
 
 --     De cada categoria s'emmagatzema un identificador únic i un nom. Una comanda és gestionada per una única botiga i una botiga pot gestionar moltes comandes. De cada botiga s'emmagatzema un identificador únic: Adreça, codi postal, localitat, província.
 
-
 --     En una botiga poden treballar molts empleats/des i un empleat/da només pot treballar en una botiga. De cada empleat/da, s'emmagatzema un identificador únic: Nom, cognoms, NIF, Telèfon.
+
 --     Si treballa com a cuiner/a o repartidor/a. Per a les comandes de repartiment a domicili interessa guardar qui és el repartidor/a que fa el lliurament de la comanda i la data/hora del moment del lliurament.
 
 DROP DATABASE IF EXISTS pizzeria;
@@ -140,10 +138,15 @@ INSERT INTO Empleats (nom, cognom1, cognom2, nif, telefon, funcio, id_botiga) VA
 ('John','Locke','Iturralde','73732471Z','590600464','repartidor',3);
 
 INSERT INTO Comandes (data_hora, tipus, numero_pizzes, numero_hamburgueses, numero_begudes, preu, id_client, id_botiga, id_empleat) VALUES
-();
+('2022-12-27 12:57:17','domicili',1,2,2,30.25,1,2,1),
+('2022-10-14 23:14:09','botiga',2,1,1,29.00,4,3,1),
+('2022-01-30 09:25:46','botiga',1,1,2,27.75,3,1,2);
 
 INSERT INTO Hamburgueses (nom, descripcio, imatge, preu, id_comanda) VALUES
-();
+('HAM-HAM','La millor hamburguesa del món','hamburguesa.jpg',5.00,1),
+('Hambu','La preferida dels peques','hambu.jpg',3.00,3),
+('Super Ham','Hamburguesa de qualitat','super-ham.jpg',4.00,2),
+('Ham Fun','Una hamburguesa super divertida','ham-fun.jpg',6.00,1);
 
 INSERT INTO Begudes (nom, descripcio, imatge, preu, id_comanda) VALUES
 ('Coca-cola','El refresc ideal per treure la set','coca_cola.jpg',6.80,1),
@@ -153,17 +156,17 @@ INSERT INTO Begudes (nom, descripcio, imatge, preu, id_comanda) VALUES
 ('Suc taronja','Vitamina C per un tub','taronja.jpg',5.00,1);
 
 INSERT INTO Pizzes (nom, descripcio, imatge, preu, id_comanda, id_categoria) VALUES
-('Ristorante Carbonara','Una deliciosa pizza de carbonara','pizza_carbonara.jpg',7.45,1,1);
-('Ristorante Margarita','Una deliciosa pizza de margarita','margarita.jpg',9.70,1,2);
-('Dr.Oetker Margarita','Una exquisita pizza de margarita','margarita2.jpg',9.30,1,2);
+('Ristorante Carbonara','Una deliciosa pizza de carbonara','pizza_carbonara.jpg',7.45,2,1),
+('Ristorante Margarita','Una deliciosa pizza de margarita','margarita.jpg',9.70,3,3),
+('Dr.Oetker Margarita','Una exquisita pizza de margarita','margarita2.jpg',9.30,2,2),
 ('Dr.Oetker quatre formatges','Una exquisita pizza de quatre formatges','quatre-formatges.jpg',6.15,1,3);
 
 -- Queries pizzeria:
 
 --     Llista quants productes de tipus “Begudes”. s'han venut en una determinada localitat.
 
-SELECT COUNT(Comandes.numero_begudes) FROM Comandes JOIN
+SELECT SUM(Comandes.numero_begudes) AS numero_begudes,clients.localitat FROM Comandes JOIN CLients ON Comandes.id_client = Clients.id WHERE Clients.localitat = "Hospitalet de Llobregat";
 
---     Llista quantes comandes ha efectuat un determinat empleat/da.
+-- --     Llista quantes comandes ha efectuat un determinat empleat/da.
 
-SELECT COUNT(*) FROM Comandes WHERE id_empleat = 1;
+SELECT COUNT(Comandes.id) AS numero_comandes,Comandes.id_empleat FROM Comandes WHERE id_empleat = 1;
